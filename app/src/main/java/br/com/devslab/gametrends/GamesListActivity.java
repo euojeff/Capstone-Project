eb15718393ba864e84b99aa14443a1a30966ce2e
euojeff <jeff.daraujo@gmail.com>
2019-01-03 23:50:55 -0300

    initial project

---
+++ app/src/main/java/br/com/devslab/gametrends/GamesListActivity.java
@@ -0,0 +1,13 @@
+package br.com.devslab.gametrends;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GamesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_games_list);
+    }
+}
